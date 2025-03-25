73646ea4f542fb17177f8321984cd4b15dfda7d0
Arshnoor19 <arshnoorsinghkairon@gmail.com>
2025-03-25 20:06:28 +0530

    changed the hero section and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-section">
-          <p>
-            DISCLAIMER – Subject to credit approval, loans are made by Bank of
-            Lake Mills or MPOWER Financing, PBC. Bank of Lake Mills does not
-            have an ownership interest in MPOWER Financing. Neither MPOWER
-            Financing nor Bank of Lake Mills is affiliated with the school you
-            attended or are attending. Bank of Lake Mills is Member FDIC. None
-            of the information contained in this website constitutes a
-            recommendation, solicitation or offer by MPOWER Financing or its
-            affiliates to buy or sell any securities or other financial
-            instruments or other assets or provide any investment advice or
-            service.
-          </p>
+          <p>DISCLAIMER – some DISCLAIMER</p>
 
           <h3>Indian Office Location </h3>
           <ul>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,17 +10,15 @@ const HeroSection = () => {
         </h1>
         <p className="hero-subtitle">Live your dream.</p>
         <p className="hero-description">
-          Apply now for a loan to study in the U.S. or Canada and make your
-          study abroad plans a reality!
+          Apply now for a loan to study and make your study abroad plans a
+          reality!
         </p>
         <button className="cta-button">GET STARTED</button>
         <p className="hero-note">
           Start the new year with <strong>low rates!</strong>
         </p>
         <p className="hero-award">
-          <strong>
-            NerdWallet 2024 Best Student Loan for International Students
-          </strong>
+          <strong>Some TEXT</strong>
         </p>
       </div>
     </section>
